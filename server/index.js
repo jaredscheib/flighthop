4ecb56878f6bafa2c94610b52f7f5c6e1fe0cafc
jaredscheib <jared.scheib@gmail.com>
2017-01-13 13:36:31 -0800

    Add basic middleware for cors and parsing request body

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.listen(process.env.PORT || 3000);
