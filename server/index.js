5809f935b110a8f045061057772cad200f90bfa7
jaredscheib <jared.scheib@gmail.com>
2017-01-13 13:47:40 -0800

    Serve static assets, refactor port

--- server/index.js
+++ server/index.js
@@ -7,4 +7,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 3000);
+app.use(express.static(`${__dirname}/public`));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}..`);
+});
