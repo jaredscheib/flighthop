89a8a928a08771a48f1aa7b1bbd8f2335343105e
jaredscheib <jared.scheib@gmail.com>
2017-01-15 20:41:37 -0800

    Authorize api requests by validating fb access token, clean up, refactor login and getflights for clarity

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -8,11 +9,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config();
 
+const fbIsTokenValid = (accessToken) => {
+  const appId = process.env.FB_APP;
+  const appSecret = process.env.FB_SECRET;
+  const path = `https://graph.facebook.com/debug_token?input_token=${accessToken}&access_token=${appId}|${appSecret}`;
+
+  return axios.get(path)
+  .then((resp) => {
+    console.log('FB access token is valid?', resp.data.data.is_valid);
+    return resp.data.data.is_valid;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+};
+
 app.use(express.static(`${__dirname}/public`));
 
+app.post('/login', (req, res) => {
+  console.log('/login');
+  fbIsTokenValid(req.body.accessToken)
+  .then((isValid) => {
+    if (isValid) res.sendStatus(200);
+    else res.sendStatus(401);
+  });
+});
+
 app.get('/flight', (req, res) => {
-  console.log(req.query);
-  res.send();
+  console.log('/flight', req.query);
+  fbIsTokenValid(req.query.accessToken)
+  .then((isValid) => {
+    if (isValid) {
+      console.log('Authorized request. Getting flight data..');
+    } else {
+      console.log('Unauthorized request.');
+      res.sendStatus(401);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
